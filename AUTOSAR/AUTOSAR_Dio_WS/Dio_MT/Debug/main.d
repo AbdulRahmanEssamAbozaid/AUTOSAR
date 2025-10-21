7f9469f727d9c15ba9cec9226a66421096f0a412
Abdo Essam Abozaid <abdoessamabozaid@gmail.com>
2025-10-21 09:02:00 +0300

    DIO for AUTOSAR Ports

---
+++ AUTOSAR/AUTOSAR_Dio_WS/Dio_MT/Debug/main.d
@@ -0,0 +1,39 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/assert.h
+main.obj: C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h
+main.obj: C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h
+main.obj: ../Dio.h
+main.obj: ../Std_Types.h
+main.obj: ../Platform_Types.h
+main.obj: ../Compiler.h
+main.obj: ../Dio_Cfg.h
+main.obj: ../Common_Macros.h
+main.obj: ../Det.h
+main.obj: ../tm4c123gh6pm_registers.h
+
+../main.c:
+
+C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/assert.h:
+
+C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h:
+
+C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h:
+
+../Dio.h:
+
+../Std_Types.h:
+
+../Platform_Types.h:
+
+../Compiler.h:
+
+../Dio_Cfg.h:
+
+../Common_Macros.h:
+
+../Det.h:
+
+../tm4c123gh6pm_registers.h:
+
